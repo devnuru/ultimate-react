55cbda5dc078f1b21902f60ff5ac0a4a6a70efca
Md Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2026-01-04 23:02:18 +0600

    Rendering the root component and strict mode

---
+++ 03-pizza-menu/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+function App() {
+  return <h1>Pizza Menu</h1>
+}
+
+// Render the App component to the DOM
+// React v18 and earlier
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);     
+
+//React before 18
+// React.render(<App />)
+
+
